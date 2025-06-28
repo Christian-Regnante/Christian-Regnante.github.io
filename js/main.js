c3da6b9577f158caadef61f171f3147aefbebc1d
christian-regnante <c.ngwino@alustudent.com>
2025-06-28 05:05:04 +0200

    Created a canvas of dots that follows the mouse cursor.

--- js/main.js
+++ js/main.js
@@ -13,15 +13,6 @@ menuClose.addEventListener('click', () => {
     navMenu_holder.classList.remove('show-menu')
 })
 
-// ===== MENU SHOW =====
-// Validate if constant exists
-
-// ===== MENU HIDDEN =====
-// Validate if constant exists
-
-
-// =============== REMOVE MENU IN MOBILE ===============
-
 // =============== CHANGE BACKGROUND HEADER ===============
 const scrollHeader = () => {
     const header = document.getElementById('header')
@@ -124,4 +115,68 @@ const contactForm = document.getElementById('contact-form'),
 
     contactForm.addEventListener('submit', sendEmail)
 
-// SCROLL REVEAL ANIMATION
+// CANVAS MOUSE ON DRAW
+
+let banner = document.querySelector('.banner');
+let canvas = document.querySelector('.dotsCanvas');
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+const ctx = canvas.getContext('2d');
+const dots = [];
+const arrayColors = ['#0d1136', '#9cb1c8', '#160435', '#d9d9e7', '#3436d2'];
+for (let index = 0; index < 80; index++) {
+    dots.push({
+        x:  Math.floor(Math.random() * canvas.width),
+        y:  Math.floor(Math.random() * canvas.height),
+        size: Math.random() * 3 + 5,
+        color: arrayColors[Math.floor(Math.random()* 5)]
+    });
+}
+const drawDots = () => {
+    dots.forEach(dot => {
+        ctx.fillStyle = dot.color;
+        ctx.beginPath();
+        ctx.arc(dot.x, dot.y, dot.size, 0, Math.PI*2);
+        ctx.fill();
+    })
+}
+drawDots();
+banner.addEventListener('mousemove', (event) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDots();
+    let mouse = {
+        x:  event.pageX - banner.getBoundingClientRect().left,
+        y:  event.pageY - banner.getBoundingClientRect().top
+    }
+    dots.forEach(dot => {
+        let distance = Math.sqrt((mouse.x - dot.x) ** 2 + (mouse.y - dot.y) ** 2);
+        if(distance < 300){
+            ctx.strokeStyle = dot.color;
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(dot.x, dot.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+    })
+})
+banner.addEventListener('mouseout', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDots();
+})
+window.addEventListener('resize', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = banner.offsetWidth;
+    canvas.height = banner.offsetHeight;
+
+    dots = [];
+    for (let index = 0; index < 50; index++) {
+        dots.push({
+            x:  Math.floor(Math.random() * canvas.width),
+            y:  Math.floor(Math.random() * canvas.height),
+            size: Math.random() * 3 + 5,
+            color: arrayColors[Math.floor(Math.random()* 5)]
+        });
+    }
+    drawDots();
+})
